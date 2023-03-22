51fbd71dbd38e6648b3a2efd073776a2dc33945b
Omotola Omotayo <126764765+Omomotoly@users.noreply.github.com>
2023-03-22 03:53:26 +0000

    add objects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hi coyote, let's have some JavaScript fun!")
+console.log('The Platform is an App for Creating and Managing Events')
+
+//I need two main objects: Users, Events and attendees, and users can create events and attendees will join the events
+//I need to be able to create a user, create an event, and create an attendee
+//I need to be able to get a list of all users, get a list of all events
+//I need to be able to get a list of all events a user has created, get a list of attendees for an event
+//Attendees should be able to join an event, and users should be able to leave an event
+//User should be able to delete an event, delete an attendee, and delete a user
+//I need to be able to edit their account information
+//I need to be able to edit an event information
+//Attendees should be to have a calender for the events they are attending
+//Attendees should be able to update their calender
+//Attendees should be able to delete an event from their calender
