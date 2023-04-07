5da646bfdc08a5a5f36f65ff7fa36b94caba06bc
Omotola Omotayo <126764765+Omomotoly@users.noreply.github.com>
2023-04-07 07:36:24 +0000

    add to database

---
+++ src/database-connection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING).then(() => console.log('Connected to MongoDB'))
+
+const Cat = mongoose.model('Cat', { name: String })
+
+/* const kitty = new Cat({ name: 'Pixel' })
+kitty.save().then(() => console.log('meow')) */
