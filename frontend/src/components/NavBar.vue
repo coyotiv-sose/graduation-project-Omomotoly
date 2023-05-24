028555313fced920e13a937d9693f59414c45148
Omotola Omotayo <126764765+Omomotoly@users.noreply.github.com>
2023-05-24 14:21:53 +0000

    add logout method in navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,21 +1,27 @@
 <script>
+import { useAccountStore } from '../stores/account'
+import { mapActions } from 'pinia'
 export default {
   name: 'NavBar',
   props: {
     user: Object
+  },
+  methods: {
+    ...mapActions(useAccountStore, ['logout'])
   }
+
 }
 </script>
 <template lang="pug">
 nav.navbar.navbar-expand-lg.bg-body-tertiary
   .container-fluid
-    a.navbar-brand(href='#') The Platform
+    a.navbar-brand(href='/') The Platform
     button.navbar-toggler(type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation')
       span.navbar-toggler-icon
     #navbarSupportedContent.collapse.navbar-collapse
       ul.navbar-nav.me-auto.mb-2.mb-lg-0
         li.nav-item
-          a.nav-link.active(aria-current='page' href='#') Home
+          router-link.nav-link.active(to="/") Home
         li.nav-item
           router-link.nav-link(to="/conferences") Conferences
         li.nav-item
--- frontend/src/views/UserView.vue
+++ frontend/src/views/UserView.vue
@@ -1,3 +1,28 @@
+<script>
+import axios from 'axios'
+axios.defaults.baseURL = import.meta.env.VITE_API_URL
+axios.defaults.withCredentials = true
+export default {
+name: 'UserView',
+data () {
+return {
+user: null
+}
+},
+
+async mounted () {
+  const user = (await axios.get(`/users/${this.$route.params.id}`)).data
+  this.user = user
+
+}
+}
+</script>
+
 <template>
-  <h1>User details</h1>
+  <h3>User details</h3>
+  <div v-if="user">
+    <p>Name: {{ user.name }}</p>
+    <h5>Conferences</h5>
+    <p  v-for="conference in user.conferences" :key="conference._id"> {{ conference.name }}</p>
+  </div>
 </template>
